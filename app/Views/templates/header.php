a6f357af467e7f6b9043a7a3253c03bb82dc02ee
Roberto Arciniegas <rarci003@fiu.edu>
2024-02-29 09:37:05 -0500

    Initial phase

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -8,7 +8,7 @@
     <title>Document</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="#">CI4 Login</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,4 +27,3 @@
         </div>
     </div>
     </nav>
-    
