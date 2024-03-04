88be30eff14131a1ce637ad4916e2a12aa4a2fb7
Roberto Arciniegas <rarci003@fiu.edu>
2024-03-04 17:53:45 -0500

    FIlters

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -24,6 +24,9 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'auth'          => \App\Filters\Auth::class,
+        'noauth'        => \App\Filters\Noauth::class,
+
     ];
 
     /**
--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,9 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Users::index');
+$routes->get('/', 'Users::index', ['filter' => 'noauth']);
 $routes->post('/', 'Users::index');
 $routes->get('logout', 'Users::logout');
-$routes->match(['get','post'],'register', 'Users::register');
-$routes->match(['get','post'],'profile', 'Users::profile');
-$routes->get('dashboard', 'Dashboard::index');
+$routes->match(['get','post'],'register', 'Users::register', ['filter' => 'noauth']);
+$routes->match(['get','post'],'profile', 'Users::profile', ['filter' => 'auth']);
+$routes->get('dashboard', 'Dashboard::index', ['filter' => 'auth']);
